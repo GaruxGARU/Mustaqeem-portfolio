32a23553efe717a706af48bd165a8d4986d5ad72
Mustaqeem <blur855@gmail.com>
2025-04-23 01:53:48 +0800

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base:"/Mustaqeem-portfolio",
+  base:"/Mustaqeem-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
